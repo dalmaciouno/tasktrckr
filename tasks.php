516c5b5dc8e9823b3268898c6ed59394b4e3b77b
Admin <CarloKalbo@gmail.com>
2026-07-13 09:23:41 +0800

    Add priority tags to tasks

--- tasks.php
+++ tasks.php
@@ -8,9 +8,10 @@ return json_decode(file_get_contents($dataFile), true);
 function saveTasks($dataFile, $tasks) {
 file_put_contents($dataFile, json_encode($tasks, JSON_PRETTY_PRINT));
 }
-function addTask(&$tasks, $title) {
+function addTask(&$tasks, $title, $priority = 3) {
 $newId = count($tasks) ? max(array_column($tasks, 'id')) + 1 : 1;
-$task = ["id" => $newId, "title" => $title, "status" => "open"];
+$task = ["id" => $newId, "title" => $title, "status" => "open", "priority" =>
+$priority];
 $tasks[] = $task;
 return $task;
 }
@@ -22,7 +23,8 @@ echo json_encode($tasks);
 break;
 case 'add':
 $input = json_decode(file_get_contents('php://input'), true);
-$task = addTask($tasks, $input['title']);
+$priority = $input['priority'] ?? 3;
+$task = addTask($tasks, $input['title'], $priority);
 saveTasks($dataFile, $tasks);
 echo json_encode($task);
 break;
